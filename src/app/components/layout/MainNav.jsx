e1a61d4c4289b471d77bf517c14c8e2a06346685
chauquach1 <chau268@gmail.com>
2024-10-07 16:37:13 -0700

    updated navbar css

---
+++ src/app/components/layout/MainNav.jsx
@@ -0,0 +1,35 @@
+"use client"
+
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  NavbarMenuToggle,
+  NavbarMenu,
+  NavbarMenuItem,
+} from "@nextui-org/navbar";
+import {Link} from "@nextui-org/link";
+import {Button} from "@nextui-org/button";
+import localFont from 'next/font/local';
+
+const fussion = localFont({
+  src: '../../fonts/Fussion.ttf',
+  display: 'swap',
+})
+
+
+export default function MainNav() {
+  return (
+    <Navbar>
+      <NavbarBrand>
+        
+      </NavbarBrand>
+      <NavbarContent className="hidden sm:flex flex-row w-full " justify="center">
+        <NavbarItem>
+          <h1 className={fussion.className + " text-gray-400"}>{"PINTECH"}</h1>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  );
+}
