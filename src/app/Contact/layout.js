822c70a879f2a511c887f00931f5b9d2e35971ae
chauquach1 <chau268@gmail.com>
2024-10-08 01:29:42 -0700

    added modal

--- src/app/Contact/layout.js
+++
@@ -1,7 +0,0 @@
-export default function ContactLayout({ children }) {
-  return (
-    <section>
-      {children}
-    </section>
-  );
-}
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/components/(button|link|navbar|ripple|spinner).js"
+    "./node_modules/@nextui-org/theme/dist/components/(button|link|modal|navbar|ripple|spinner).js"
   ],
   theme: {
     extend: {
